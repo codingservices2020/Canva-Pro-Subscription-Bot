5b41b32b66ae7d3c7a16ec7bbc5e668657604403
codingservices2020 <codingservices2020@gmail.com>
2025-01-16 21:52:26 +0530

    Update CanvaPro.py

--- CanvaPro.py
+++ CanvaPro.py
@@ -9,10 +9,13 @@ import os
 from apscheduler.schedulers.background import BackgroundScheduler
 import asyncio
 
+from keep_alive import keep_alive
+keep_alive()
+
 # from dotenv import load_dotenv
 # load_dotenv()
 
-keep_alive()
+
 
 TOKEN = os.getenv('TOKEN')
 PRIVATE_CHANNEL_ID = int(os.getenv('PRIVATE_CHANNEL_ID'))
